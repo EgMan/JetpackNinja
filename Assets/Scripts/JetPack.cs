905e9f8146e0e0f1f8dca497b688079f8e652fff
Antonio Woods <antonio95woods@gmail.com>
2020-10-06 09:55:42 -0400

    Add fuel mechanic as well as HUD for fuel and thrust

--- Assets/Scripts/JetPack.cs
+++ Assets/Scripts/JetPack.cs
@@ -1,23 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JetPack : MonoBehaviour
 {
+    public float maxFuel;
+    public Slider FuelBar, ThrustBar;
+    public Text FuelLabel, ThrustLabel;
     public float thrustMultiplier = 25f;
     public AudioSource soundEffect;
     private Rigidbody rb;
     private CharacterController character;
+    private float currentFuel;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         character = gameObject.GetComponent<CharacterController>();
+        currentFuel = maxFuel;
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
+
         float jetpackInput = OVRInput.Get(OVRInput.RawAxis1D.LIndexTrigger);
-        rb.AddForce(new Vector3(0, jetpackInput * thrustMultiplier, 0), ForceMode.Force);
-        soundEffect.volume = jetpackInput;
+        if (currentFuel > 0)
+        {
+            //Thrust
+            rb.AddForce(new Vector3(0, jetpackInput * thrustMultiplier, 0), ForceMode.Force);
+            soundEffect.volume = jetpackInput;
+            ThrustBar.value = Mathf.Floor(jetpackInput * 100);
+            ThrustLabel.text = $"Thrust {ThrustBar.value}%";
+
+            //Fuel
+            currentFuel -= jetpackInput;
+            FuelBar.value = Mathf.Floor((100 * currentFuel) / maxFuel);
+            FuelLabel.text = $"Fuel {FuelBar.value}%";
+        }
+        else
+        {
+            soundEffect.volume = 0;
+            ThrustBar.value = 0;
+            ThrustLabel.text = "Thrust 0%";
+
+            currentFuel = 0;
+            FuelBar.value = 0;
+            FuelLabel.text = "Fuel 0%";
+        }
+
+
 
         if (Input.GetKey(KeyCode.J))
         {
